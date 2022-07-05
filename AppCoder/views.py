2acc3dab234f1a6bf23b7047913f95f7e7b05f99
MTouzon <matias.l.t@hotmail.com>
2022-07-05 19:29:12 -0300

    commit inicial clase 20

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -4,9 +4,29 @@ from AppCoder.models import Curso
 from django.http import HttpResponse
 # Create your views here.
 
-def curso(self):
+def curso(request):
 
     curso = Curso(nombre="Django", comision=45364)
     curso.save()
     texto = f"Curso creado: {curso.nombre} {curso.comision}"
     return HttpResponse(texto)
+
+def inicio(request):
+
+    return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+
+    return render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+
+    return render(request, "AppCoder/entregables.html")
